ac22131ecf3e11bd00d49eb359fae4b5bd3878c6
Ulf <ulf.rustas@gmail.com>
2021-06-17 22:23:01 +0200

    Add vcs_data field

--- README.md
+++ README.md
@@ -2,14 +2,17 @@ python3 -m http.server --bind 0.0.0.0 9999
 
 # TODO:
 - Layout
-    - palette
+    - palette (of rooms)
 - Editor
+    - Select drawable/fixed clear/fixed filled pf0,pf1,pf2
     - Draw lines
     - Draw boxes
     - Draw circle
     - Copy/paste
 - Map
+    . Edit level
+    - Create / select separate levels
 - Data
-    - read-only editor
-    - Export / Import
+    - Select include pf0,pf1,pf2
     - Local storage
+    - Export / Import
---
+++ vcs_data.js
@@ -0,0 +1,38 @@
+export class VCSData {
+  constructor(textarea, playfield) {
+    this.textarea = textarea;
+    this.playfield = playfield;
+  }
+
+
+  updateFromPlayfield() {
+    const playfieldData = this.playfield.data;
+    const getPaddedByte = (num) => num.toString(2).padStart(8,'0')
+    let text = ''
+
+    playfieldData.forEach((row) => {
+      let PF0 = 0;
+      let PF1 = 0;
+      let PF2 = 0;
+      row.forEach((value, x) => {
+        // Both NORMAL and REFLECTED only use half the width.
+        // PFO |PF1     |PF2
+        // 4567|76543210|01234567
+        if (value) {
+          if (x <= 3) {
+            PF0 += 1 << (x + 4);
+          } else if (x <= 11) {
+            PF1 += 1 << (7 - (x - 4));
+          } else if (x <= 19) {
+            PF2 += 1 << (x - 12);
+          }
+        }
+      });
+      // .byte #%00100000, #%11000100,
+      const rowData = `.byte #%${getPaddedByte(PF0)}, #%${getPaddedByte(PF1)}, #%${getPaddedByte(PF2)}\n`
+      text += rowData
+    });
+
+    this.textarea.value = text
+  }
+}
