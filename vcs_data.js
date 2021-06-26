19ace5e38f3d6cb3ed6c19a65ae029e444a3f143
Ulf Rustas <ulf.rustas@gmail.com>
2021-06-26 14:44:38 +0200

    Select registers to include in data

--- vcs_data.js
+++ vcs_data.js
@@ -1,9 +1,20 @@
 export class VCSData {
+
+  #includeRegisters
+
   constructor(textarea, playfield) {
     this.textarea = textarea;
     this.playfield = playfield;
+    this.#includeRegisters =Array(3).fill(true)
   }
 
+  setIncludeRegister(register, include) {
+    if (register < 0 || register > 2) {
+      throw new Error("Weird register " + register)
+    }
+
+    this.#includeRegisters[register] = !!include
+  }
 
   updateFromPlayfield() {
     const playfieldData = this.playfield.data;
@@ -11,26 +22,31 @@ export class VCSData {
     let text = ''
 
     playfieldData.forEach((row) => {
-      let PF0 = 0;
-      let PF1 = 0;
-      let PF2 = 0;
+      const PF = [0,0,0]
       row.forEach((value, x) => {
         // Both NORMAL and REFLECTED only use half the width.
         // PFO |PF1     |PF2
         // 4567|76543210|01234567
         if (value === 1) {
           if (x <= 3) {
-            PF0 += 1 << (x + 4);
+            PF[0] += 1 << (x + 4);
           } else if (x <= 11) {
-            PF1 += 1 << (7 - (x - 4));
+            PF[1] += 1 << (7 - (x - 4));
           } else if (x <= 19) {
-            PF2 += 1 << (x - 12);
+            PF[2] += 1 << (x - 12);
           }
         }
       });
       // .byte #%00100000, #%11000100,
-      const rowData = `.byte #%${getPaddedByte(PF0)}, #%${getPaddedByte(PF1)}, #%${getPaddedByte(PF2)}\n`
-      text += rowData
+      // const rowData = `.byte #%${getPaddedByte(PF0)}, #%${getPaddedByte(PF1)}, #%${getPaddedByte(PF2)}\n`
+      const rowData = []
+      for (let i = 0; i<=2; ++i) {
+        if (this.#includeRegisters[i]) {
+          rowData.push(`#%`+getPaddedByte(PF[i]))
+        }
+      }
+
+      text += `.byte ${rowData.join(', ')}\n`
     });
 
     this.textarea.value = text
